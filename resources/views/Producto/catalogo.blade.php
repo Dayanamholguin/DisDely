15201c78ef6d75d823f4971c423694799c794f49
Isabel <icloaiza28@misena.edu.co>
2022-07-20 22:04:49 -0500

    Arreglos de Catálogo

--- resources/views/Producto/catalogo.blade.php
+++ resources/views/Producto/catalogo.blade.php
@@ -15,9 +15,9 @@
 
 @if ($usuarioEnSesion->hasRole('Admin')==false)
 @can('agregarCarrito')
-    @section('car')
-        @include('carrito.icono')
-    @endsection
+@section('car')
+@include('carrito.icono')
+@endsection
 @endcan
 @endif
 @section('content')
@@ -30,7 +30,8 @@
             <p>Acá podrás encontrar los productos registrados en la plataforma, podrás visualizarlos, ver el detalle y
                 si así lo quieres ¡Cotizar!<br>
                 @can('cotizacion/personalizada')
-                <a href="/cotizacion/personalizada" class="alert-link titulo">¿Deseas cotizar un producto personalizado? Clic aquí</a>
+                <a href="/cotizacion/personalizada" class="alert-link titulo">¿Deseas cotizar un producto personalizado?
+                    Clic aquí</a>
                 @endcan
             </p>
         </div>
@@ -44,151 +45,149 @@
 
         <!--Search-->
         <style>
-            .hol {
-                display: flex;
-                align-items: center;
-            }
+        .hol {
+            display: flex;
+            align-items: center;
+        }
 
-            form>div {
-                display: flex;
-                background: #fff;
-                padding: 9px 22px 9px 26px;
-                border-radius: 30px;
-                border: 2px solid #cad3dc;
-                box-shadow: rgba(255, 255, 255, 0.5) -8px -8px 15px,
-                    rgba(0, 0, 0, 0.1) 10px 10px 10px,
-                    rgba(255, 255, 255, 0.5) -8px -8px 15px inset,
-                    rgba(0, 0, 0, 0.1) 10px 10px 10px inset;
-            }
+        form>div {
+            display: flex;
+            background: #fff;
+            padding: 9px 22px 9px 26px;
+            border-radius: 30px;
+            border: 2px solid #cad3dc;
+            box-shadow: rgba(255, 255, 255, 0.5) -8px -8px 15px,
+                rgba(0, 0, 0, 0.1) 10px 10px 10px,
+                rgba(255, 255, 255, 0.5) -8px -8px 15px inset,
+                rgba(0, 0, 0, 0.1) 10px 10px 10px inset;
+        }
 
-            form input {
-                border: none;
-                background: transparent;
-                font-weight: bold;
-                padding-left: 24px;
-                background-size: 16px;
-                width: 0px;
-                transition: all 1s;
-            }
+        form input {
+            border: none;
+            background: transparent;
+            font-weight: bold;
+            padding-left: 24px;
+            background-size: 16px;
+            width: 0px;
+            transition: all 1s;
+        }
 
-            form input:focus {
-                outline: none;
-                width: 250px;
-            }
+        form input:focus {
+            outline: none;
+            width: 250px;
+        }
         </style>
 
         <form class="hol">
             <div>
                 <i class="bi bi-search"></i>
-                <input type="text" name="" id="buscar" placeholder="Buscar..." >
+                <input type="text" name="" id="buscar" placeholder="Buscar...">
             </div>
         </form>
 
         <br>
 
-        <div class="row portfolio-container product-list" id="product-list" data-aos="fade-up" data-aos-delay="200">
-            @foreach($productos as $producto)
-            <div class="col-lg-4 col-md-6 item portfolio-item filter-app product ">
-                <div class="portfolio-img"><a href="/ver/imagen/{{$producto->id}}" data-gall="portfolioGallery" class="venobox preview-link" title="{{$producto->nombre}}"><img style="background-size: 100% 100%;" src="/imagenes/{{$producto->img}}" class="img-fluid" alt=""></a></div>
-                <div class="portfolio-info">
-                    <h4 class="nombres">{{$producto->nombre}}</h4>
-
-                    <p>{{ucfirst(Date::create($producto->created_at)->format('F j, Y'));}}</p>
-                    @can('cotizacion/crear')
-                        <a href="/cotizacion/crear/{{$producto->id}}" class=" preview-link"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAFxJREFUSEtjZKAxYKSx+QyjFhAMYZKC6P////9BJjIyMhKtj2iFIINHLSAYYUMniGAuJeglNAXYUhfWVERzC3C5fOjEwagPkENgcJVFpOYLcMlLjiZS9IxaQDC0AFaEOBlExtG1AAAAAElFTkSuQmCC" /></a>
-                    @endcan
-                    @can('producto/verProductoCatalogo')
-                        <a href="/producto/verProductoCatalogo/{{$producto->id}}" class="details-link"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVVJREFUSEvVVVFRA0EUSxSABFBA6wAcgAJwADgAB8UBOMABoIA6oBKogjBh3nYe7e4thSsz7Mx9XPc2yea9lxI7XtwxPpoEkg4AXAI4BjAJIXMAzwDuSC6+I65KIGkW4EMYM5LXPZINAklWeRQHHwAYyL9Bkm9yBeC83IjkdIjkC0FSvrQ1BXgdIIhs1V7YZdLqWhGE52/x1bQFXlCC5DXeD1s1yQTFdxewqSjLlHQfdjXPZILifVd95RbzVi0ygXyQ5FazIWnw3FgES5L7tSqPZdELSQ/kxvrTIjsaftKmnplJt01jUkurvgM46QzaEwD7fkvypjtoqfVyVLjP3eM5KhyAFwnQexZjUe0arA1QL+xsi785jdxqkvTi2hPt7nD4GdRAfhyAC0m2yJnk/SrJVkNVs6BH8muCaI58kzOSj0XMKASJxBG/Av+MnqE/izH2/j/BB3p8rBlg04KKAAAAAElFTkSuQmCC" /></a>
-                    @endcan
-                </div>
-            </div>
-            @endforeach
-            <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                    <div class="portfolio-img"><img src="/assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt=""></div>
+        <div class="row product-list" id="product-list" data-aos="fade-up" data-aos-delay="200">
+            <div class="contenedor-galeria">
+                @foreach($productos as $producto)
+                <div class="col-md-12 galeria__img item portfolio-item filter-app product ">
+                    <div class="portfolio-img"><a href="/ver/imagen/{{$producto->id}}" data-gall="portfolioGallery"
+                            class="venobox preview-link" title="{{$producto->nombre}}"><img
+                                style="background-size: 100% 100%;" src="/imagenes/{{$producto->img}}" class="img-fluid"
+                                alt=""></a></div>
                     <div class="portfolio-info">
-                        <h4>Web 3</h4>
-                        <p>Web</p>
-                        <a href="/assets/img/portfolio/portfolio-2.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        <h4 class="nombres">{{$producto->nombre}}</h4>
+
+                        <p>{{ucfirst(Date::create($producto->created_at)->format('F j, Y'));}}</p>
+                        @can('cotizacion/crear')
+                        <a href="/cotizacion/crear/{{$producto->id}}" class=" preview-link"><img
+                                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAFxJREFUSEtjZKAxYKSx+QyjFhAMYZKC6P////9BJjIyMhKtj2iFIINHLSAYYUMniGAuJeglNAXYUhfWVERzC3C5fOjEwagPkENgcJVFpOYLcMlLjiZS9IxaQDC0AFaEOBlExtG1AAAAAElFTkSuQmCC" /></a>
+                        @endcan
+                        @can('producto/verProductoCatalogo')
+                        <a href="/producto/verProductoCatalogo/{{$producto->id}}" class="details-link"><img
+                                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVVJREFUSEvVVVFRA0EUSxSABFBA6wAcgAJwADgAB8UBOMABoIA6oBKogjBh3nYe7e4thSsz7Mx9XPc2yea9lxI7XtwxPpoEkg4AXAI4BjAJIXMAzwDuSC6+I65KIGkW4EMYM5LXPZINAklWeRQHHwAYyL9Bkm9yBeC83IjkdIjkC0FSvrQ1BXgdIIhs1V7YZdLqWhGE52/x1bQFXlCC5DXeD1s1yQTFdxewqSjLlHQfdjXPZILifVd95RbzVi0ygXyQ5FazIWnw3FgES5L7tSqPZdELSQ/kxvrTIjsaftKmnplJt01jUkurvgM46QzaEwD7fkvypjtoqfVyVLjP3eM5KhyAFwnQexZjUe0arA1QL+xsi785jdxqkvTi2hPt7nD4GdRAfhyAC0m2yJnk/SrJVkNVs6BH8muCaI58kzOSj0XMKASJxBG/Av+MnqE/izH2/j/BB3p8rBlg04KKAAAAAElFTkSuQmCC" /></a>
+                        @endcan
                     </div>
-                </div>-->
+                </div>
+                @endforeach
+            </div>
+            @endif
         </div>
-        @endif
     </div>
 </section><!-- End Portfolio Section -->
 @endsection
 
 @section('scripts')
 <script>
-    /*  let pro = <?= json_encode($productos); ?>;
+/*  let pro = <?= json_encode($productos); ?>;
     console.log(pro); */
 
-    // document.addEventListener("keyup", e =>{
-        
-    //     if (e.target.matches("#buscar")) {
-    //         document.querySelectorAll(".nombres").forEach(producto => {
-    //             console.log(producto.textContent);
-    //             producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-    //             ?producto.classList.remove("filtro")
-    //             :producto.classList.add("filtro")
-    //         });
-    //     }
-    // })
+// document.addEventListener("keyup", e =>{
+
+//     if (e.target.matches("#buscar")) {
+//         document.querySelectorAll(".nombres").forEach(producto => {
+//             console.log(producto.textContent);
+//             producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+//             ?producto.classList.remove("filtro")
+//             :producto.classList.add("filtro")
+//         });
+//     }
+// })
 
 
-    $(document).ready(function(){
-        $('#buscar').keyup(function(){
-            var nombres = $('.nombres');
-            var buscando = $(this).val();
-            var item='';
-            for( var i = 0; i < nombres.length; i++ ){
-                item = $(nombres[i]).html().toLowerCase();
-                for(var x = 0; x < item.length; x++ ){
-                    if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
-                        $(nombres[i]).parents('.item').show();
-                    }else{
-                        $(nombres[i]).parents('.item').hide();
-                    }
+$(document).ready(function() {
+    $('#buscar').keyup(function() {
+        var nombres = $('.nombres');
+        var buscando = $(this).val();
+        var item = '';
+        for (var i = 0; i < nombres.length; i++) {
+            item = $(nombres[i]).html().toLowerCase();
+            for (var x = 0; x < item.length; x++) {
+                if (buscando.length == 0 || item.indexOf(buscando) > -1) {
+                    $(nombres[i]).parents('.item').show();
+                } else {
+                    $(nombres[i]).parents('.item').hide();
                 }
             }
-        });
+        }
     });
-    
-    
-    //Search
-    // const search = () => {
-    //     const searchbox = document.getElementById("searh-item").value.toUpperCase();
-    //     const storeitems = document.getElementById("product-list");
-    //     const product = document.querySelectorAll(".product");
-    //     const pname = storeitems.getElementsByTagName("h4");
+});
 
-    //     for (var i = 0; i < pname.length; i++) {
-    //         let match = product[i].getElementsByTagName('h4')[0];
 
-    //         if (match) {
-    //             let textvalue = match.textContent || match.innerHTML
+//Search
+// const search = () => {
+//     const searchbox = document.getElementById("searh-item").value.toUpperCase();
+//     const storeitems = document.getElementById("product-list");
+//     const product = document.querySelectorAll(".product");
+//     const pname = storeitems.getElementsByTagName("h4");
 
-    //             if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-    //                 product[i].style.display = "";
-    //             } else {
-    //                 product[i].style.display = "none";
-    //             }
-    //         }
-    //     }
-    // }
-    //end search
+//     for (var i = 0; i < pname.length; i++) {
+//         let match = product[i].getElementsByTagName('h4')[0];
 
-    // $.ajax({
-    //     url: `/producto/catalogo/${idCategoria}`,
-    //     type: "GET",
-    //     success: function (res){
-    //         let productos = JSON.parse(res)
-    //         productos.forEach(p => {
-    //             document.querySelector(".contenedor").appendChild(`<p>nombre: ${p.nombre}</p>`
-    //             )
-    //         })
-    //     },
-    // });
+//         if (match) {
+//             let textvalue = match.textContent || match.innerHTML
+
+//             if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+//                 product[i].style.display = "";
+//             } else {
+//                 product[i].style.display = "none";
+//             }
+//         }
+//     }
+// }
+//end search
+
+// $.ajax({
+//     url: `/producto/catalogo/${idCategoria}`,
+//     type: "GET",
+//     success: function (res){
+//         let productos = JSON.parse(res)
+//         productos.forEach(p => {
+//             document.querySelector(".contenedor").appendChild(`<p>nombre: ${p.nombre}</p>`
+//             )
+//         })
+//     },
+// });
 </script>
 
 <!-- <script>
